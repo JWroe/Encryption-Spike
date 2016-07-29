5cf85a125e8e48cd2badeeb69a0f8798d6739064
Jamie <Jamie.Wroe@live.co.uk>
2016-07-29 18:39:20 +0100

    key generator to create private and public RSA keys

---
+++ EncryptionSpike/KeyGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                var publicKey = rsa.ExportCspBlob(includePrivateParameters: false);
+                var privateKey = rsa.ExportCspBlob(includePrivateParameters: true);
+                File.WriteAllBytes(@".\public-key.rsa", publicKey);
+                File.WriteAllBytes(@".\private-key.rsa", privateKey);
+            }
+            Console.WriteLine("New keys generated. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
